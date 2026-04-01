33d9445668b625f867c71d5cb8f24d699112a644
ShaniaMtz <sxm210198@utdallas.edu>
2026-03-31 20:03:20 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,40 @@
 let grid = document.querySelectorAll(".grid-container"); 
+let divs = document.querySelectorAll(".grid-container div");
+
+function randomize () {
+  console.log(divs.length, "randomize!")
+
+  //affects the whole grid
+  grid[0].style.transform = 'rotate(${Math.random() *
+    360}deg'
+
+  //just the grid divs
+  let scale = randomNumber(.5, 1.2);
+  let translateX = randomNumber(0, 50); 
+  let translateY = randomNumber(0,20);
+  let rotate = randomNumber (0, 60); 
+
+  //50% of the time
+  if (Math. random() >.5) {
+    scale = 1; //doesnt affect scale
+    translateX = randomNumber(0, 80):
+    translateY = randomNumber(0, 80); 
+  } 
+
+  div.style.transform = 'scale(${scale}) translate($
+  {translateX}%,${translateY}%) rotate(${rotate}deg)';
+
+}
+
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) +
+  min; 
+}
+
+
+
 
 
 
